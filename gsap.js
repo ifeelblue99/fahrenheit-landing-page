3bdf0f78168d36a14b6a5784e8366e0ad3a3cb17
Baran Cakirbey <barancakirbey13@gmail.com>
2022-02-14 13:41:14 +0300

    hero section improved

--- gsap.js
+++ gsap.js
@@ -8,3 +8,12 @@ gsap.from("#header", {
     opacity: 0,
     scale: 0.1
 });
+
+//hero chair image
+gsap.from("#hero-img", {
+    duration: 2.5,
+    x: -3000,
+    ease: "elastic.out(1, .9)",
+    opacity: 0,
+    scale: 0.1
+});
